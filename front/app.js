4d5245b4e2fc67ba4c819c07ca0ae7841e274fae
Nora Sadder <norasadder12@gmail.com>
2023-11-05 16:15:52 +0200

    front server built with some modifications on the catalog and order servers

--- front/app.js
+++ front/app.js
@@ -1,28 +1,69 @@
 const express = require("express");
 const axios = require("axios");
-
 const app = express();
-const port = 3001;
+const port = 3000;
+
+app.get("/purchase/:item_number", async (req, res) => {
+  try {
+    const itemToPurchase = req.params.item_number;
+    const orderServerUrl = "http://order:3000";
+    const queryResponse = await axios.get(
+      `${orderServerUrl}/purchase/${itemToPurchase}`
+    );
+    if (queryResponse.status === 200) {
+      res.json(queryResponse.data);
+    } else {
+      res.status(500).json({ error: "Unexpected response from order server" });
+    }
+  } catch (error) {
+    // Handle any errors that occur during the purchase process
+    console.error(error);
+    res.status(500).json({ error: "Error processing the purchase" });
+  }
+});
+
+app.get("/info/:item_number", async (req, res) => {
+  try {
+    const itemToQuery = req.params.item_number;
+    const catalogServerUrl = "http://catalog:3000";
+    const queryResponse = await axios.get(
+      `${catalogServerUrl}/query/${itemToQuery}`
+    );
+    if (queryResponse.status === 200) {
+      res.json(queryResponse.data);
+    } else {
+      res
+        .status(500)
+        .json({ error: "Unexpected response from catalog server" });
+    }
+  } catch (error) {
+    // Handle any errors that occur during the purchase process
+    console.error(error);
+    res.status(500).json({ error: "Error processing the query" });
+  }
+});
 
 app.get("/search/:topic", async (req, res) => {
   try {
     const topicToSearch = req.params.topic;
-    // Replace 'target-url' with the URL you want to send a GET request to
-    const response = await axios.get(
-      `http://localhost:3000/subject/${topicToSearch}`
+    const catalogServerUrl = "http://catalog:3000";
+    const queryResponse = await axios.get(
+      `${catalogServerUrl}/subject/${topicToSearch}`
     );
-
-    if (response === "No results found.") {
-      res.send("No results found.");
+    if (queryResponse.status === 200) {
+      res.json(queryResponse.data);
+    } else {
+      res
+        .status(500)
+        .json({ error: "Unexpected response from catalog server" });
     }
-
-    res.send(response.data); // Send the response from the target URL to the client
   } catch (error) {
+    // Handle any errors that occur during the purchase process
     console.error(error);
-    res.status(500).send("Error making the request");
+    res.status(500).json({ error: "Error processing the query" });
   }
 });
 
 app.listen(port, () => {
-  console.log("Server is running on port", port);
+  console.log(`Express server is running on port ${port}`);
 });
